09e3760306ffdf23bffc40bec9a8c1fcb45beb70
marcincakA <marcincaka@gmail.com>
2024-01-21 23:57:41 +0100

    game page and edit review

--- resources/views/myList.blade.php
+++ resources/views/myList.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
     <title>MyList</title>
 </head>
 <body>
@@ -63,10 +65,10 @@
         </div>
     </nav>
 </header>
-<div class="container-fluid text-center">
+<div class="container-fluid text-center" id = "myListHeader">
     <h1>My gaming list</h1>
 </div>
-<div class="table-responsive-md">
+<div class="table-responsive-md" id="myListTable">
     <table class="table">
         <thead>
         <tr>
